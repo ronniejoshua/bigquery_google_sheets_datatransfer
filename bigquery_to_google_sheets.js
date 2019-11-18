5af4701a2b2d951ca041eb0d071dc96e76c92dfb
Ronnie Joshua <ron.juden@gmail.com>
2019-11-18 23:42:16 +0200

    Update bigquery_to_google_sheets.js

--- bigquery_to_google_sheets.js
+++ bigquery_to_google_sheets.js
@@ -1,20 +1,18 @@
 /*
-Google Sheet Format
-+--------------------+---------------------+-------------------+
-|      Tab Name      |        Query        |    Description    |
-+--------------------+---------------------+-------------------+
-| my_gsheet_tab_name | starndard_sql_query | query_description |
-+--------------------+---------------------+-------------------+
- */ 
++--------------------+---------------------+
+|      Tab Name      |        Query        |
++--------------------+---------------------+
+| my_gsheet_tab_name | starndard_sql_query |
++--------------------+---------------------+
+ */
 
-/* BigQuery Project ID */
-PROJECT_ID = 'gcp-bigquery-project-id';
+ /* BigQuery Project ID */
+PROJECT_ID = 'long-grin-524';
 
 function onOpen(e) {
     var menu = SpreadsheetApp.getUi().createMenu("Read BQ");
     menu.addSeparator()
     menu.addItem("Run Query", 'run_query');
-
     menu.addToUi();
 }
 
@@ -73,26 +71,49 @@ function save_into_spreadsheet(query, tab_name) {
         sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(tab_name)
     }
 
-    // Crear the tab and past the new data
+    // Clear the tab and paste the new data
     sheet.getDataRange().clearContent()
     sheet.getRange(1, 1, values.length, values[0].length).setValues(values);
 }
+
+
+function highlightDuplicates() {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("Settings");
+    var num_rows = sheet.getLastRow() - 1;
+    var data = sheet.getRange(2, 1, num_rows, 2).sort(1).getValues();
+    var reset_backgroup = sheet.getRange(2, 1, num_rows, 2).setBackground("white")
+    var duplicates_count = 0;
+    for (var i in data) {
+      var row = data[i];
+      for (var j in data){
+        if(i!=j && row[0] == data[j][0]){
+          sheet.getRange("A"+(parseInt(i)+2)+":B"+(parseInt(i)+2)).setBackground("#ed6666");
+          duplicates_count++
+        }
+      }
+    }
+   return [data, duplicates_count]
+  }
+
 
 /**
  * Gets the information about the query to run and fire the query
  */
 function run_query() {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = ss.getSheetByName("Settings")
-    var num_rows = sheet.getLastRow() - 1;
-    Logger.log(num_rows)
-    var query_data = sheet.getRange(2, 1, num_rows, 2).getValues();
-  for (var i in query_data){
-    Logger.log(i)
-    var query_name = query_data[i][0];
-    var query = query_data[i][1];
-    Logger.log(query_name)
-    Logger.log(query)
-    save_into_spreadsheet(query, query_name)
-  }
+     var query_objects = highlightDuplicates();
+     var query_data = query_objects[0];
+     Logger.log(query_data)
+     if (query_objects[1] == 0){
+       for (var i in query_data){
+         var query_name = query_data[i][0];
+         var query = query_data[i][1];
+         Logger.log(query_name)
+         Logger.log(query)
+         save_into_spreadsheet(query, query_name)
+         } 
+     } else {
+       var ui = SpreadsheetApp.getUi();
+       ui.alert("ALERT: Please Check - There are duplicates in 'Tab Name' Columns")
+     }
 }
